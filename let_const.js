e129e56d0825b65e483bf71dc93c8230e0e9e01e
RanjithKumar <ranjithkumarpatil@gmail.com>
2022-09-02 22:29:16 +0530

    let and const keywords in ES6

---
+++ let_const.js
@@ -0,0 +1,14 @@
+var myName = "Ranjith";
+console.log(myName);
+
+myName = "Rajesh";
+console.log(myName);
+
+let age = 21;
+console.log(age);
+
+age = 22;
+console.log(age);
+
+const dob = 1998;
+console.log(dob);
